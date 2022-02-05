29b446db8ff8f7aaab2adbe23ef078648b2d7dc1
Leonel Aguirre <leonel.aguirre@hbr.org>
2022-02-05 12:20:12 -0600

    P5 canvas working.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./index.css"
+
+import p5 from "p5"
+
+const WIDTH = 700
+const HEIGHT = 700
+
+const sketch = (p) => {
+  // Canvas setup.
+  p.setup = () => {
+    let canvas = p.createCanvas(WIDTH, HEIGHT)
+    canvas.parent("Canvas")
+  }
+
+  // Draw loop.
+  p.draw = () => {
+    p.background(200)
+  }
+}
+
+new p5(sketch)
