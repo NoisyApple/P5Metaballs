eab639b9ba6af92bec0283fb63b7657d705db5e8
Leonel Aguirre <leonel.aguirre@hbr.org>
2022-02-05 13:12:49 -0600

    Point array kinda works.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,30 @@ import p5 from "p5"
 const WIDTH = 700
 const HEIGHT = 700
 
+const RESOLUTION = 25
+
+let pointArray = new Array(WIDTH / RESOLUTION + 1)
+
+for (let i = 0; i < pointArray.length; i++)
+  pointArray[i] = new Array(HEIGHT / RESOLUTION + 1)
+
+for (let i = 0; i < pointArray.length; i++)
+  for (let j = 0; j < pointArray[i].length; j++)
+    pointArray[i][j] = Math.floor(Math.random() * 2)
+
+// TODO: Create a class based Point object which can be initialized with an x and y position and a value
+
 const sketch = (p) => {
+  const drawGrid = () => {
+    for (let i = 0; i <= WIDTH / RESOLUTION; i++) {
+      for (let j = 0; j <= HEIGHT / RESOLUTION; j++) {
+        p.stroke(pointArray[i][j] ? "black" : "white")
+        p.strokeWeight(10)
+        p.point(i * RESOLUTION, j * RESOLUTION)
+      }
+    }
+  }
+
   // Canvas setup.
   p.setup = () => {
     let canvas = p.createCanvas(WIDTH, HEIGHT)
@@ -14,7 +37,8 @@ const sketch = (p) => {
 
   // Draw loop.
   p.draw = () => {
-    p.background(200)
+    p.background("#888")
+    drawGrid()
   }
 }
 
